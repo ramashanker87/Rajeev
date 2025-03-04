c2eecda96bffd83f44b2c037527167e4cf808e73
admin <sairajeev.tulluri12@gmail.com>
2025-03-04 13:12:27 +0530

    Day03

---
+++ Day03/java-collection/src/main/java/com/rama/app/list/ListSamples.java
@@ -0,0 +1,39 @@
+package com.rama.app.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListSamples {
+    public static List<String> stringList = new ArrayList<String>();
+    public void add(String data){
+        stringList.add(data);
+    }
+    public void printList(){
+        for(String data : stringList){
+            System.out.println(data);
+        }
+    }
+    public void remove(String data){
+        stringList.remove(data);
+    }
+    public void sizeOfList(){
+        System.out.println(stringList.size());
+    }
+    public static void main(String[] args) {
+        ListSamples listSamples = new ListSamples();
+        listSamples.add("test1"); // Index 0
+        listSamples.add("test2"); // Index 1
+        listSamples.add("test3"); // Index 2
+        System.out.println("Value at Index 1:"+stringList.get(1));
+        stringList.set(1,"Rajeev");
+        System.out.println("Value at Index 1:"+stringList.get(1));
+        listSamples.printList();
+        listSamples.sizeOfList();
+        System.out.println("After Remove");
+        listSamples.remove("test2");
+        listSamples.printList();
+        listSamples.sizeOfList();
+        listSamples.add("test4");
+        System.out.println(stringList.get(2));
+    }
+}
