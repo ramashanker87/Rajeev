685ecf90d543012374154d11f100445631062aef
admin <sairajeev.tulluri12@gmail.com>
2025-03-06 16:59:19 +0530

    Day03

---
+++ Day03/java-collection/src/main/java/com/rama/app/map/MapSamples.java
@@ -0,0 +1,50 @@
+package com.rama.app.map;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MapSamples {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap();
+        map.put("A", 1);
+        map.put("B", 2);
+        map.put("c", 3);
+        map.put("d", 4);
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+        map.remove("A");
+        System.out.println("After remove A");
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+        Map<String, Integer> map2 = new LinkedHashMap();
+        map.put("A", 1);
+        map.put("B", 2);
+        map.put("c", 3);
+        map.put("d", 4);
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+        map.remove("A");
+        System.out.println("After remove A");
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+        Map<String, Integer> map3 = new TreeMap<>();
+        map.put("A", 1);
+        map.put("B", 2);
+        map.put("c", 3);
+        map.put("d", 4);
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+        map.remove("A");
+        System.out.println("After remove A");
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+}
